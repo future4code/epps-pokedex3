60b3d26fd8e5100b368ec3e3ceea42b0a495469b
Igor Chacon <igorchacon@yahoo.com.br>
2021-02-13 19:07:29 -0300

    feita pokedex

--- pokedex/src/componentes/PokemonCard/PokemonCard.js
+++ pokedex/src/componentes/PokemonCard/PokemonCard.js
@@ -15,9 +15,28 @@ const PokemonCard = (props) => {
 
     const history = useHistory();
 
-    const { pokemons, setPokemons } = useContext(
+    const { pokemons, setPokemons, pokedex, setPokedex } = useContext(
         GlobalStateContext
     )
+
+    const AddToPokedex = () => {
+        const pokeIndex = pokemons.findIndex(
+            (item) => item.name === props.poke.name
+        )
+        const newPokeList = [...pokemons]
+        newPokeList.splice(pokeIndex, 1)
+        const orderedPokemons = newPokeList.sort((a, b) => {
+            return a.id = b.id
+        })
+
+        const newPokedexList = [...pokedex, props.poke]
+        const orderedPokedex = newPokedexList.sort((a, b) => {
+            return a.id - b.id
+        })
+
+        setPokedex(orderedPokedex)
+        setPokemons(orderedPokemons)
+    }
 
     return (
         <CardContainer>
@@ -29,7 +48,7 @@ const PokemonCard = (props) => {
             <NameText>{props.name}</NameText>
             <PriceContainer>
                 <ButtonsContainer>
-                    <button>Adicionar a Pokedex</button>
+                    <button onClick={AddToPokedex}>Adicionar a Pokedex</button>
                     <button
                         onClick={() =>
                             goToDetailsScreen(history, props.poke.name)
