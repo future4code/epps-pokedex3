321f8d1e641dc273d30bebb716afcbff88d1c927
Igor Chacon <igorchacon@yahoo.com.br>
2021-02-13 21:15:01 -0300

    modificação na pokedex

--- pokedex/src/screens/PokedexUserScreen/PokedexUserScreen.js
+++ pokedex/src/screens/PokedexUserScreen/PokedexUserScreen.js
@@ -24,6 +24,7 @@ const PokedexUserScreen = () => {
                                 />
                     })}
             </PokeListContainer>
+            <button onClick={() => goToHomeScreen(history)}>Voltar para lista de Pokemons</button>
         </>
     )
 }
